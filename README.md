4986ec32cff21e2e000c089d80abdf9b2a8d63d9
Robertarezza <roberta.rezza@gmail.com>
2024-04-29 15:03:29 +0200

    add: bonus

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 
 ## Bonus
-- [ ]Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+- [x]Far comparire gli indirizzi email solamente quando sono stati tutti generati.
--- js/script.js
+++ js/script.js
@@ -4,18 +4,28 @@ createApp({
   data() {
     return {
      email: [],
+     emailsGeneratorAll: true,
     }
   },
   methods: {
     generateMails: function () {
+        this.emailsGeneratorAll= false;
+        this.email=[];
         for (let i = 0; i < 10; i++ ) {
         axios
         .get("https://flynn.boolean.careers/exercises/api/random/mail")
         .then((resp) => {
           //console.log(resp.data.response);
+         
           this.email.push(resp.data.response);
+          console.log(this.email)
+          
+          if (this.email.length === 10) {
+            this.emailsGeneratorAll = true
+          }
+          
         });
     }
-    }
+    },
   }
 }).mount('#app')
